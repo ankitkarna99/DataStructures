cd22f8215ef777239e3dd0d898d83cf52e8db83a
Ankit Karna <ankit.karna.771@gmail.com>
2018-09-26 15:31:56 +0545

    Added stack file

--- stack.cpp
+++ stack.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-#include "singlyLinkedList.hpp";
+#include "singlyLinkedList.hpp"
 
 using namespace std;
 
+template <class T>
+class Stack {
+    private:
+        LinkedList<T> list;
+    public:
+        void push(T data){
+            list.addFront(data);
+        }
+        T pop(){
+            T temp = list.getFront();
+            list.removeFront();
+            return temp;
+        }
+        T peek(){
+            return list.getFront();
+        }
+        int stackSize() {
+            return list.getSize();
+        }
+        void printStack(){
+            cout << "Printing stack from top to bottom:" << endl;
+            list.printList();
+        }
+        bool isEmpty(){
+            return (list.getSize() == 0) ? true : false;
+        }
+};
+
 int main(){
+    Stack<int> intStack;
+    cout << "Empty: " << intStack.isEmpty() << endl;
+    intStack.push(5);
+    intStack.push(6);
+    intStack.push(7);
+    intStack.push(8);
+    cout << "Empty: " << intStack.isEmpty() << endl;
+    intStack.printStack();
+    cout << "Popped: " << intStack.pop() << endl;
+    cout << "Peeking: " << intStack.peek() << endl;
+    cout << "Stack Size: " << intStack.stackSize() << endl;
     return 0;
 }
