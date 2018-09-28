3e7dc583d04986390ab025ac9424da0e8916badb
Ankit Karna <ankit.karna.771@gmail.com>
2018-09-28 17:59:36 +0545

    Updated stack cpp file

--- stack.cpp
+++ stack.cpp
@@ -1,35 +1,7 @@
 #include <iostream>
-#include "singlyLinkedList.hpp"
+#include "stack.hpp"
 
 using namespace std;
-
-template <class T>
-class Stack {
-    private:
-        LinkedList<T> list;
-    public:
-        void push(T data){
-            list.addFront(data);
-        }
-        T pop(){
-            T temp = list.getFront();
-            list.removeFront();
-            return temp;
-        }
-        T peek(){
-            return list.getFront();
-        }
-        int stackSize() {
-            return list.getSize();
-        }
-        void printStack(){
-            cout << "Printing stack from top to bottom:" << endl;
-            list.printList();
-        }
-        bool isEmpty(){
-            return (list.getSize() == 0) ? true : false;
-        }
-};
 
 int main(){
     Stack<int> intStack;
